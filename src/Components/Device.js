31eab0961128ed3c82125b433fa89467c1a49837
avijitsaha118 <sahaavijit908@gmail.com>
2022-03-21 11:41:40 +0600

    updated

--- src/Components/Device.js
+++ src/Components/Device.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import DeviceDetail from './DeviceDetail/DeviceDetail';
+import Tablet from './Tablet/Tablet';
 
 const Device = (props) => {
     return (
         <div>
             <h2>I have: {props.name}</h2>
             <DeviceDetail price={props.price}></DeviceDetail>
+            <Tablet></Tablet>
         </div>
     );
 };
---
+++ src/Components/Tablet/Tablet.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Tablet extends Component {
+    render() {
+        return (
+            <div>
+                <h5>My simple watch:</h5>
+            </div>
+        );
+    }
+}
+
+export default Tablet;
